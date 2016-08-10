470f8c955d87abf16d77b4b188755d71f224cff6
Shirai-Kuroko <freebakas@national.shitposting.agency>
2016-08-09 23:30:06 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <div class="currentlyplaying">
 
   <p id="songname">hello</p>
-    <p id="player"><audio class="stream" src = test.mp3 controls></p>
+    <p id="player"><audio class="stream" src = lain.mp3.m3u controls></p>
   <p id="info">
 </div>
 
